1cfbdf1bb056d81d7875ae72623fc3ec745dbb3a
Jacob DeRosa <50190964+jakederosa123@users.noreply.github.com>
2021-03-21 02:39:35 -0400

    Necessary Libraries

---
+++ bagpype_libraries.py
@@ -0,0 +1,18 @@
+pip install pandas 
+pip install sklearn
+pip install matplotlib
+pip install numpy
+pip install scipy
+pip install sklearn
+pip install pandas 
+pip install warnings
+pip install seaborn
+pip install python-igraph
+pip install subprocess
+pip install pyyaml
+python3 -m pip install ipykernel
+python3 -m ipykernel install --user 
+brew install parallel 
+
+
+
