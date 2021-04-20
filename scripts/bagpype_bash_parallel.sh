da3c2116b9a0011d77b9e4780bc8da74b6ccb3a9
Jacob DeRosa <50190964+jakederosa123@users.noreply.github.com>
2021-04-19 22:11:52 -0400

    Parallel Files Update

--- scripts/bagpype_bash_parallel.sh
+++ scripts/bagpype_bash_parallel.sh
@@ -1,8 +1,7 @@
 #!bin/bashs
 batch=$1
-path=$(grep 'data_path:' bagpype_template.yaml | tail -n1 | cut -c 13-)
-pype=$(grep 'pype_path:' bagpype_template.yaml | tail -n1 | cut -c 13-)
-cors=$(grep 'cors:' bagpype_template.yaml | tail -n1 | cut -c7-)
+path=$(grep 'data_path:' "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/bagpype_template.yaml | tail -n1 | cut -c 13-)
+pype=$(grep 'pype_path:' "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/bagpype_template.yaml | tail -n1 | cut -c 13-)
+cors=$(grep 'cors:' "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/bagpype_template.yaml | tail -n1 | cut -c7-)
 cd "/$pype/BagPype/scripts"
 cat "/$path"/${batch}/Output/Results/temp_txtfile.txt | parallel -j "$cors" "python3 /"$pype"/BagPype/scripts/bagpype_clust_function.py {} "${batch}"" 2> ${batch}_log.txt
-
