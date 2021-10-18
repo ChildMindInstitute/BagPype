4c82ef6e59c4a34851e8406a9259766ff6950a60
Jacob DeRosa <50190964+jakederosa123@users.noreply.github.com>
2021-10-17 20:17:17 -0600

    Update bagpype_clust_function.py

--- scripts/bagpype_clust_function.py
+++ scripts/bagpype_clust_function.py
@@ -50,16 +50,3 @@ res2.to_csv(f'{outfolder}/Output/Results/Boot_Q/%s_%s.csv' % (batch, i))
 #data3 = np.hstack(Silo.reshape(-1,1))
 #res3 = pd.DataFrame(data=data3)
 #res3.to_csv(f'{outfolder}/Output/Results/Boot_Silo/%s_%s.csv' % (batch, i))
-#=======================================================================================================#
-### Beta testing for Variable Clusering ### 
-#X_split = df.iloc[b_idx[i],:]
-#X_split = X_split.T
-#X_split = X_split.iloc[1:]
-#print('Lacing up right shoe bootstrap #%d' % (i + 1))
-#communities2, Q2 = pheno_clust(X=np.array(X_split), verbose=True, distance = distance_metric)
-#data1 = np.hstack(communities2.reshape(-1,1))
-#res1 = pd.DataFrame(data=data1)
-#res1 = res1.rename(columns={0: '1'})
-#res1[0] = list(range(len(res1)))
-#res1 = res1.iloc[:,[1,0]]
-#res1.to_csv(f'{outfolder}/Output/data/Boot/%s_%s.csv' % (batch, i))
